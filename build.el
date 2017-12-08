3b6da22edf50061506af1ff0ec489791356fca25
Timo Lassmann <timolassmann@icloud.com>
2017-12-08 18:42:07 +0800

    seems to work with emacsclient -a <file>

---
+++ build.el
@@ -0,0 +1,104 @@
+(require 'org)         ;; The org-mode goodness
+(require 'ob)          ;; org-mode export system
+(require 'ob-tangle)   ;; org-mode tangling process
+
+
+(message "starting  loading scripts.")
+
+
+(defconst dot-files-src  (file-name-directory (or load-file-name buffer-file-name)))
+(message "The name of this buffer is: %s." dot-files-src)
+
+
+(defvar script-funcs-src (concat dot-files-src
+"elisp/shell-script-funcs.el"))
+
+
+(require 'shell-script-funcs script-funcs-src)
+
+(defconst tl/emacs-directory (concat (getenv "HOME") "/.emacs.d/"))
+
+;; Where all of the .el files will live and play:
+(defconst dest-elisp-dir (tl/get-path "${tl/emacs-directory}/elisp"))
+
+;; The Script Part ... here we do all the building and compilation work.
+
+
+
+
+
+
+(defun tl/build-dot-files ()
+  "Compile and deploy 'init files' in this directory."
+  (interactive)
+
+  ;; Initially create some of the destination directories
+  ;;(ha/mkdir "$HOME/.oh-my-zsh/themes")
+  (tl/mkdir "${tl/emacs-directory}/elisp")
+  (tl/mkdir "${tl/emacs-directory}/latex_templates") 
+
+  (tl/tangle-files "${dot-files-src}/*.org")
+
+  ;; Some Elisp files are just symlinked instead of tangled...
+  (tl/mksymlinks "${dot-files-src}/elisp/*.el"
+                 "${tl/emacs-directory}/elisp")
+
+  ;; copy my latex templates
+  (tl/mksymlinks "${dot-files-src}/latex_templates/*.sty"
+                 "${tl/emacs-directory}/latex_templates")
+  
+
+  ;; Just link the entire directory instead of copying the snippets:
+  ;;(ha/mksymlink  "${dot-files-src}/snippets"
+   ;;              "${ha/emacs-directory}/snippets")
+
+  ;; Just link the entire directory instead of copying the snippets:
+  ;;(ha/mksymlink  "${dot-files-src}/templates"
+  ;;               "${ha/emacs-directory}/templates")
+
+  ;; Some Elisp files are just symlinked instead of tangled...
+  ;;(ha/mksymlinks "${dot-files-src}/bin/[a-z]*"
+  ;;               "${HOME}/bin")
+
+  ;; Yeah, this makes me snicker every time I see it.
+  ;;(ha/mksymlink  "${dot-files-src}/vimrc" "${HOME}/.vimrc")
+
+  ;; All of the .el files I've eithe tangled or linked should be comp'd:
+  ;; (mapc 'byte-compile-file
+  ;;       (ha/get-files "${ha/emacs-directory}/elisp/*.el" t))
+
+  (message "Finished building dot-files- Resetting Emacs.")
+ (require 'config-main (tl/get-path "${user-emacs-directory}elisp/config-main.el")))
+
+
+
+(defun tl/tangle-file (file)
+  "Given an 'org-mode' FILE, tangle the source code."
+  (interactive "fOrg File: ")
+  (find-file file)   ;;  (expand-file-name file \"$DIR\")
+  (org-babel-tangle)
+  (kill-buffer))
+
+
+(defun tl/tangle-files (path)
+  "Given a directory, PATH, of 'org-mode' files, tangle source code out of all literate programming files."
+  (interactive "D")
+  (message "The name of this buffer is: %s." (tl/get-files path))
+  (mapc 'tl/tangle-file (tl/get-files path)))
+
+
+;;(defun ha/get-dot-files ()
+;; "Pull and build latest from the Github repository.  Load the resulting Lisp code."
+;;  (interactive)
+;;  (let ((git-results
+;;         (shell-command (concat "cd " dot-files-src "; git pull"))))
+;;    (if (not (= git-results 0))
+;;        (message "Can't pull the goodness. Pull from git by hand.")
+ ;;     (load-file (concat dot-files-src "/emacs.d/shell-script-funcs.el"))
+ ;;     (load-file (concat dot-files-src "/build.el"))
+;;      (require 'init-main))))
+
+(tl/build-dot-files)  ;; Do it
+
+(provide 'dot-files)
+;;; build.el ends here
---
+++ elisp/shell-script-funcs.el
@@ -0,0 +1,130 @@
+;;; shell-script-funcs --- Utility functions for shell script conversions
+
+;;; Commentary:
+
+;; A collection of functions helpful in attempting to translate shell
+;; scripts into Elisp scripts.
+
+(require 'em-glob)
+
+;;; Code:
+
+(defun tl/substring-replace (old-str new-str beg end)
+  "Return a new string where a subsection of OLD-STR has been replaced with NEW-STR beginning at position BEG and ending at END."
+   (concat (substring old-str 0 beg) new-str (substring old-str end)))
+
+
+(defun tl/getvar (var-name)
+  "Return value of a variable or environment variable specified by VAR-NAME."
+  (or (getenv var-name) (eval (read var-name))))
+
+(defun tl/substr-variables (str)
+  "Replace shell-like '$VAR' and '${variables}' in STR with the equivalent environment variables or Elisp variables.  For instance: $HOME/.emacs.d could return /home/howard/.emacs.d -- Keep in mind that this is just a string, it does not do any validation to see if any files exist."
+
+  ;; This function recursively calls this function with more and more
+  ;; embedded variables substituted out, until no more variables are
+  ;; found, and then it returns the results.
+  ;;
+  ;; Begin by checking to see if the string starts with ~ ...
+  (if (string-prefix-p "~/" str)
+      (tl/substr-variables
+       (concat (getenv "HOME") (substring str 1)))
+
+    ;; Variables can either be simple $BLAH or ${some-larger}...
+    (let ((s (or (string-match "${\\([^ }]*\\)}" str)
+                 (string-match "$\\([A-z_]*\\)" str)))
+          (e (match-end 0)))
+      (if (not s)             ; No $ matches?
+          str                 ; Then just return the string.
+        (tl/substr-variables  ; Recursively call with first var sub'd
+         (tl/substring-replace str (tl/getvar (match-string 1 str)) s e))))))
+
+;; (ha/substr-variables "$HOME/.emacs.d/elisp/*.el")
+;; (ha/substr-variables "~/.emacs.d/elisp/*.el")
+;; (ha/substr-variables "${user-emacs-directory}elisp/*.el")
+
+(defun tl/get-files (path &optional full)
+  "Return list of files that match the glob pattern, PATH.  Allowing shell-like variable substitution from the environment, like $HOME, or from variables defined by `setq'.  If FULL is specified, return absolute pathnames for each file."
+  (let ((subbed-path (tl/substr-variables path)))
+    (condition-case nil
+        (directory-files (file-name-directory subbed-path)
+                         full
+                         (eshell-glob-regexp
+                          (file-name-nondirectory subbed-path)))
+      (error '()))))
+
+;; (ha/get-files "$HOME/.emacs.d/elisp/*.el")
+;; => ("init-blog.el" "init-client.el" "init-clojure.el" ...)
+
+;; (ha/get-files "$HOME/.emacs.d/elisp/*.el" t)
+;; => ("/home/howard/.emacs.d/elisp/init-blog.el" "/home/howard/.emacs.d/elisp/init-client.el" ...)
+
+;; (ha/get-files "${user-emacs-directory}/elisp/*.el")
+;; => ("init-blog.el" "init-client.el" "init-clojure.el" ...)
+
+;; (ha/get-files "/foo/bar/*")  => nil
+
+(defun tl/has-org-tag-p (file tag)
+  (let ((reg (concat "^#\\+TAGS:.* " tag "\\b")))
+    (with-temp-buffer
+      (insert-file-contents file)
+      (re-search-forward reg nil t 1))))
+
+(defun tl/get-org-files (path tag &optional full)
+  "Return list of `org-mode' files in PATH that match the TAG section.
+If FULL is specified, return absolute pathnames for each file."
+  (filter (lambda (file) (tl/has-org-tag-p file tag)) (tl/get-files path full)))
+
+;; (ha/get-org-files "~/website/Presentations/*.org" "presentation" t)
+
+(defun tl/get-path (path &rest extra)
+  "Return a file specification based on PATH.  We should expand this function so that glob patterns work when specifying the parent, but shouldn't worry about matching any particular file.  All EXTRA parameters are appended separated with / characters."
+  (let ((file-parts (cons (tl/substr-variables path) extra)))
+    (mapconcat 'identity file-parts "/")))
+
+;; (ha/get-path "/home/bar") ;=> "/home/bar"
+;; (ha/get-path "$HOME/.emacs.d/elisp/*.el") ;=> "/home/howard/.emacs.d/elisp/*.el"
+;; (ha/get-path "/foo/bar" "baz")
+;;
+;; (let ((blah "blah-blah") )
+;;   (ha/get-path "/home/${blah}/flubber")) ;=> /home/blah-blah/flubber
+;;
+;; (let ((blah "blah-blah") )
+;;   (ha/get-path "/home/flubber" blah)) ;=> /home/flubber/blah-blah
+
+;; ----------------------------------------------------------------------
+
+;; The following functions are basic "shell" like functions that take
+;; a path that refers to files. This allows us to not have to call
+;; (ha/get-files) directly.
+
+(defun tl/mkdir (path)
+  "Create a directory specified by PATH, which can contain embedded environment variables and Emacs variables, e.g. '$HOME/Work/foobar'."
+  (make-directory (tl/get-path path) t))
+
+
+(defun tl/mksymlink (orig link)
+  "Create symbolic line to ORIG.  If LINK is an existing link, it is deleted first.  LINK could also refer to a directory.  Note: Both parameters are strings that can accept embedded environment and Lisp variables, e.g. '$HOME/Work/foo.el' and '${user-emacs-directory}/elisp/bar.el' ."
+  (let ((orig-file (tl/get-path orig))
+        (link-file (tl/get-path link)))
+
+    (if (not (file-symlink-p link-file))
+        (make-symbolic-link orig-file link-file t))))
+
+(defun tl/mksymlinks (files dest)
+  "Create an absolute symbolic link for each file specified in FILES into the directory, DEST (where each link will have the same name).  Both parameters can be specified with glob patterns and embedded environment and Emacs variables, e.g. '$HOME/.emacs.d/*.el'."
+  (mapc (lambda (file) (tl/mksymlink file dest)) (tl/get-files files t)))
+
+
+(defun tl/mkdirs (path)
+  "Create one or more directories specified by PATH, which can contain embedded environment variables and Emacs variables, e.g. '$HOME/Work/foobar'."
+  (mapc (lambda (dir) (make-directory dir t)) (tl/get-files path)))
+
+(defun tl/copy-files (from to files)
+  "Copy some files FROM a directory TO another directory, where FILES is a list of names."
+  (mapcar (lambda (file)
+            (copy-file (tl/get-path from file)
+                       (tl/get-path to) t))     files))
+
+(provide 'shell-script-funcs)
+;;; shell-script-funcs ends here
