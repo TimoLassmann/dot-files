e072e3317beeff2e84deff07c8d432bbef498126
Timo Lassmann <timo.lassmann@telethonkids.org.au>
2021-04-20 10:41:25 +0800

    Cleaned up eshell

--- tl-elisp/tl-cmode.el
+++ tl-elisp/tl-cmode.el
@@ -5,6 +5,8 @@
 ;; Author: Timo Lassmann <timo.lassmann@telethonkids.org.au>
 ;; Keywords: c
 
+(require 'ggtags)
+
 (defun tl/setup-c-mode ()
   (setq c-default-style "bsd")
   (setq-default tab-width 4)
@@ -16,14 +18,16 @@
     (when (derived-mode-p 'c-mode)
       (ggtags-mode 1)))
   (lambda () (add-hook 'before-save-hook 'whitespace-cleanup))
+  (lambda () (setq flycheck-clang-language-standard "c11"))
+  ;; Modes I want 
+  (ggtags-mode 1)
   (smartparens-mode 1)
   (hl-line-mode 1)
   (clean-aindent-mode 1)
   (dtrt-indent-mode 1)
+
+  ;; Modes I don't want
   (org-indent-mode -1)
   (org-roam-mode -1))
-
-
-
 
 (provide 'tl-cmode)
---
+++ tl-elisp/tl-eshell.el
@@ -0,0 +1,212 @@
+;;; tl-eshell.el --- Small utilities for eshell; mostly copied from Protesilaos Stavrou  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Timo Lassmann
+
+;; Author: Timo Lassmann <timo.lassmann@telethonkids.org.au>
+;; Keywords:
+
+
+
+;;; prot-eshell.el --- Extensions to Eshell for my dotemacs -*- lexical-binding: t -*-
+
+;; Copyright (C) 2020-2021  Protesilaos Stavrou
+
+;; Author: Protesilaos Stavrou <info@protesilaos.com>
+;; URL: https://protesilaos.com/dotemacs
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "27.1"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; This covers my Eshell extensions, for use in my Emacs setup:
+;; https://protesilaos.com/dotemacs.
+;;
+;; Remember that every piece of Elisp that I write is for my own
+;; educational and recreational purposes.  I am not a programmer and I
+;; do not recommend that you copy any of this if you are not certain of
+;; what it does.
+
+;;; Code:
+(require 'eshell)
+(require 'esh-mode)
+(require 'em-dirs)
+(require 'em-hist)
+
+;;;###autoload
+(defun prot-eshell-complete-recent-dir (&optional arg)
+  "Switch to a recent Eshell directory using completion.
+With optional ARG prefix argument (\\[universal-argument]) also
+open the directory in a `dired' buffer."
+  (interactive "P")
+  (let* ((dirs (ring-elements eshell-last-dir-ring))
+         (dir (completing-read "Switch to recent dir: " dirs nil t)))
+    (insert dir)
+    (eshell-send-input)
+    (when arg
+      (dired dir))))
+
+;;;###autoload
+(defun prot-eshell-find-subdirectory-recursive ()
+  "Recursive `eshell/cd' to subdirectory.
+This command has the potential for infinite recursion: use it
+wisely or prepare to call `eshell-interrupt-process'."
+  (interactive)
+  (let* ((dir (abbreviate-file-name (eshell/pwd)))
+         (contents (directory-files-recursively dir ".*" t nil nil))
+         (dirs (cl-remove-if-not (lambda (x)
+                                   (or (file-directory-p x)
+                                       (string-match-p "\\.git" x)))
+                                 contents))
+         (selection (completing-read
+                     (format "Find sub-dir from %s: "
+                             (propertize dir 'face 'success))
+                     dirs nil t)))
+    (insert selection)
+    (eshell-send-input)))
+;;;###autoload
+(defun prot-eshell-root-dir ()
+  "Switch to the root directory of the present project."
+  (interactive)
+  (let ((root (or (vc-root-dir)
+                  (locate-dominating-file "." ".git"))))
+    (if root
+        (progn
+          (insert root)
+          (eshell-send-input))
+      (user-error "Cannot find a project root here"))))
+
+
+;; Stolen from Howard Abrams!!
+
+(defun curr-dir-git-branch-string (pwd)
+  "Returns current git branch as a string, or the empty string if
+PWD is not in a git repo (or the git command is not found)."
+  (interactive)
+  (when (and (not (file-remote-p pwd))
+             (eshell-search-path "git")
+             (locate-dominating-file pwd ".git"))
+    (let* ((git-url (shell-command-to-string "git config --get remote.origin.url"))
+           (git-repo (file-name-base (s-trim git-url)))
+           (git-output (shell-command-to-string (concat "git rev-parse --abbrev-ref HEAD")))
+           (git-branch (s-trim git-output)))
+      (concat "(git: " git-repo ":" git-branch ") " ))))
+
+(defun pwd-replace-home (pwd)
+  "Replace home in PWD with tilde (~) character."
+  (interactive)
+  (let* ((home (expand-file-name (getenv "HOME")))
+         (home-len (length home)))
+    (if (and
+         (>= (length pwd) home-len)
+         (equal home (substring pwd 0 home-len)))
+        (concat "~" (substring pwd home-len))
+      pwd)))
+
+(defun pwd-shorten-dirs (pwd)
+  "Shorten all directory names in PWD except the last two."
+  (let ((p-lst (split-string pwd "/")))
+    (if (> (length p-lst) 2)
+        (concat
+         (mapconcat (lambda (elm) (if (zerop (length elm)) ""
+                               (substring elm 0 1)))
+                    (butlast p-lst 2)
+                    "/")
+         "/"
+         (mapconcat (lambda (elm) elm)
+                    (last p-lst 2)
+                    "/"))
+      pwd)))  ;; Otherwise, we just return the PWD
+
+(defun split-directory-prompt (directory)
+  (if (string-match-p ".*/.*" directory)
+      (list (file-name-directory directory) (file-name-base directory))
+    (list "" directory)))
+
+(defun ruby-prompt ()
+  "Returns a string (may be empty) based on the current Ruby Virtual Environment."
+  (let* ((executable "~/.rvm/bin/rvm-prompt")
+         (command    (concat executable "v g")))
+    (when (file-exists-p executable)
+      (let* ((results (shell-command-to-string executable))
+             (cleaned (string-trim results))
+             (gem     (propertize "\xe92b" 'face `(:family "alltheicons"))))
+        (when (and cleaned (not (equal cleaned "")))
+          (s-replace "ruby-" gem cleaned))))))
+
+(defun python-prompt ()
+  "Returns a string (may be empty) based on the current Python
+   Virtual Environment. Assuming the M-x command: `pyenv-mode-set'
+   has been called."
+  (when (fboundp #'pyenv-mode-version)
+    (let ((venv (pyenv-mode-version)))
+      (when venv
+        (concat
+         (propertize "\xe928" 'face `(:family "alltheicons"))
+         (pyenv-mode-version))))))
+(defun eshell/eshell-local-prompt-function ()
+  "A prompt for eshell that works locally (in that is assumes
+that it could run certain commands) in order to make a prettier,
+more-helpful local prompt."
+  (interactive)
+  (let* ((pwd        (eshell/pwd))
+         (directory (split-directory-prompt
+                     (pwd-shorten-dirs
+                      (pwd-replace-home pwd))))
+         (parent (car directory))
+         (name   (cadr directory))
+         (branch (curr-dir-git-branch-string pwd))
+         (ruby   (when (not (file-remote-p pwd)) (ruby-prompt)))
+         (python (when (not (file-remote-p pwd)) (python-prompt)))
+
+         (dark-env (eq 'dark (frame-parameter nil 'background-mode)))
+         (for-bars                 `(:weight bold))
+         (for-parent               `(:foreground "#105f89"))
+         (for-dir                  `(:foreground "#1083be" :weight bold))
+                       
+         (for-git                  `(:foreground "#2aa889" :slant italic ))
+         (for-ruby                 `(:foreground "red"))
+         (for-python               `(:foreground "#5555FF")))
+
+    (concat
+     (when branch
+       (concat (propertize ""    'face for-bars)
+               (propertize branch   'face for-git)))
+     (propertize ""    'face for-bars)
+     (propertize parent   'face for-parent)
+     (propertize name     'face for-dir)
+     
+     (when ruby
+       (concat (propertize " ── " 'face for-bars)
+               (propertize ruby   'face for-ruby)))
+     (when python
+       (concat (propertize " ── " 'face for-bars)
+               (propertize python 'face for-python)))
+     (propertize ""     'face for-bars) ;add "\n" here to have prompt on new line 
+     (propertize (if (= (user-uid) 0) " #" " $") 'face `(:foreground "#1a99e7" :weight ultra-bold))
+     ;; (propertize " └→" 'face (if (= (user-uid) 0) `(:weight ultra-bold :foreground "red") `(:weight ultra-bold)))
+     (propertize " "    'face `(:weight bold)))))
+
+(setq-default eshell-prompt-function #'eshell/eshell-local-prompt-function)
+
+(defun tl/my-eshell ()
+    (global-hl-line-mode -1)
+    )
+
+(add-hook 'eshell-mode-hook 'tl/my-eshell)
+
+(provide 'tl-eshell)
