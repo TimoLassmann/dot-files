86415dd4655fc8c589912524516ff619b3fcc9ce
Timo Lassmann <timo.lassmann@telethonkids.org.au>
2021-04-20 17:21:09 +0800

    Moved C-mode definitions to the correct hooks

--- tl-elisp/tl-cmode.el
+++ tl-elisp/tl-cmode.el
@@ -11,23 +11,9 @@
   (setq c-default-style "bsd")
   (setq-default tab-width 4)
 
-  (lambda ()
-    (set (make-local-variable 'company-backends)
-         '(company-clang  company-gtags  company-c-headers company-dabbrev )))
-  (lambda ()
-    (when (derived-mode-p 'c-mode)
-      (ggtags-mode 1)))
-  (lambda () (add-hook 'before-save-hook 'whitespace-cleanup))
-  (lambda () (setq flycheck-clang-language-standard "c11"))
-  ;; Modes I want 
-  (ggtags-mode 1)
-  (smartparens-mode 1)
-  (hl-line-mode 1)
-  (clean-aindent-mode 1)
-  (dtrt-indent-mode 1)
-
-  ;; Modes I don't want
-  (org-indent-mode -1)
-  (org-roam-mode -1))
+  (setq flycheck-clang-language-standard "c11")
+  ;; Modes I want
+  ;;(ggtags-mode 1)
+)
 
 (provide 'tl-cmode)
