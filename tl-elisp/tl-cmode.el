84d6f8d46366fa764996aa107e1e0fe91e1ea707
Timo Lassmann <timo.lassmann@telethonkids.org.au>
2021-05-03 13:58:15 +0800

    Final touches

--- tl-elisp/tl-cmode.el
+++ tl-elisp/tl-cmode.el
@@ -14,6 +14,16 @@
   (setq flycheck-clang-language-standard "c11")
   ;; Modes I want
   ;;(ggtags-mode 1)
-)
+  (setq-local company-backends
+              (append '((company-clang  company-gtags  company-c-headers company-dabbrev))))
+
+
+  ;; (smartparens-mode 1)
+  ;; (hl-line-mode 1)
+
+
+  ;; (org-indent-mode -1)
+  ;; (org-roam-mode -1)
+  )
 
 (provide 'tl-cmode)
